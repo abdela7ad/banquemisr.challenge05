26571e4c39b4d811a7c81f3e1ce23bb865ebce36
Abdelahad Darwish <abdelahad.darwish@ibm.com>
2025-01-21 09:08:48 +0200

    inital project

---
+++ Challenge05/Challenge05/Challenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge05App.swift
+//  Challenge05
+//
+//  Created by Abdelahad on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Challenge05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
